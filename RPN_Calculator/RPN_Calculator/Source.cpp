fe5f6e7666fdcdb00c6d8c04bf070d08b1da6d57
hubertleszcz <hubciol1308@gmail.com>
2025-01-25 17:31:55 +0100

    Added comments explaining the functions in the code

--- RPN_Calculator/RPN_Calculator/Source.cpp
+++ RPN_Calculator/RPN_Calculator/Source.cpp
@@ -6,10 +6,18 @@
 
 using namespace std;
 
+/*
+* Funtion that returns true if given token is a number and false when it's not
+* @param token - token to check
+*/
 bool isANumber(string token) {
 	return token[0] >= '0' && token[0] <= '9';
 }
 
+/*
+* function that returns the argument count from MIN or MAX operations
+* @param function - MIN or MAX token to check
+*/
 int getArgs(string function) {
 	string temp = "";
 
@@ -21,7 +29,10 @@ int getArgs(string function) {
 
 }
 
-
+/*
+* function that returns the priority of the operator or function
+* @param token - token to check
+*/
 int getPriority(string token) {
 	if (token == "+") {
 		return Priority::PLUS;
@@ -56,37 +67,58 @@ int getPriority(string token) {
 	}
 }
 
+/*
+* function that prints the stack from top to bottom
+* param @numbers - stack to print out
+*/
+
 void printEntireStack(stack<int> numbers) {
 
 	int n = numbers.size();
-	int* tab = new int[numbers.size()];
+	stack<int> temp;
 
 	while (!numbers.empty()) {
-		tab[numbers.size() - 1] = numbers.top();
+		cout << numbers.top() << " ";
+		temp.push(numbers.top());
 		numbers.pop();
 	}
 
-	for (int i = n - 1; i >= 0; i--) {
-		std::cout << tab[i] << " ";
+	while (!temp.empty()) {
+		numbers.push(temp.top());
+		temp.pop();
 	}
 
-	delete[] tab;
 }
 
+
+/*
+* function that makes the pop and top stack operations shorter in code
+* @param stack - stack on which the operations are performed
+*/
 template <typename T>
-T stackTopAndPop(stack <T>& numbers) {
-	T a = numbers.top();
-	numbers.pop();
+T stackTopAndPop(stack <T>& stack) {
+	T a = stack.top();
+	stack.pop();
 	return a;
 }
 
+/*
+* function which performs operations like negation, IF, MIN and MAX
+* @param op - operator
+* @param numbers - stack of numbers used in operations
+*/
+
 void functionOperations(string op, stack <int>& numbers) {
 	int a, b, c;
+	// negation
 	if (op == "N") {
 		a = stackTopAndPop(numbers);
 		numbers.push((-1) * a);
 	}
 	else if (op == "IF") {
+
+		// IF ( a , b , c ) - if ( a > 0 ) returns b, else returns c
+
 		a = stackTopAndPop(numbers);
 
 		b = stackTopAndPop(numbers);
@@ -100,7 +132,9 @@ void functionOperations(string op, stack <int>& numbers) {
 	}
 
 	else if (op[0] == 'M') {
+
 		int arguments = getArgs(op);
+		// MIN operation
 		if (op[2] == 'N') {
 			int min = 0x0FFFFFF0;
 			for (int i = 0; i < arguments; i++) {
@@ -109,6 +143,7 @@ void functionOperations(string op, stack <int>& numbers) {
 			}
 			numbers.push(min);
 		}
+		// MAX operation
 		else {
 			int max = (-1) * 0x0FFFFFF0;
 			for (int i = 0; i < arguments; i++) {
@@ -125,11 +160,15 @@ void functionOperations(string op, stack <int>& numbers) {
 
 }
 
-
+/*
+* function that performs +,-,*,/ operation and returns true if it's successful ( when it's not dividing by 0
+* @param op - operator
+* @param numbers - stack of numbers used in operations
+*/
 bool arithmeticOperation(string op, stack<int>& numbers) {
-	//std::cout << op << " ";
-	//printEntireStack(numbers);
-	//std::cout << endl;
+	std::cout << op << " ";
+	printEntireStack(numbers);
+	std::cout << endl;
 	int a, b;
 
 	if (op == "+") {
@@ -164,15 +203,20 @@ bool arithmeticOperation(string op, stack<int>& numbers) {
 	return true;
 }
 
+
+/*
+* function that returns true if given operator is a function and false if it's not
+* @param op - operator
+*/
 bool isAFuntion(string op) {
 	if (op == "IF" || op == "MIN" || op == "MAX") return true;
 	return false;
 }
 
-
-
-
-
+/*
+* function that calculates the expression in postfix notation
+* @param tokens - postfix expression
+*/
 void calculateRPN(queue <string> tokens) {
 	stack<int> numbers;
 	string currentToken;
@@ -183,18 +227,23 @@ void calculateRPN(queue <string> tokens) {
 		if (isANumber(currentToken)) {
 			numbers.push(stoi(currentToken));
 		}
-
 		else {
-
 			if (!arithmeticOperation(currentToken, numbers)) return;
 		}
 
 	}
+	// the result of the expression
 	std::cout << numbers.top() << endl << endl;
 
 }
 
-
+/*
+* function that reduces the operator's count on the stack during shunting yard algorithm. Basically it pops the operators from the stack, can possibly
+* extend them by the arguments count and pushes them to the postfix notation
+* @param operators - operators stack
+* @param functionArgs - stack for the function arguments counts
+* @param output - postfix notation
+*/
 void reduceOperators(stack<string>& operators, stack<int>& functionArgs, queue<string>& output) {
 	int temp;
 	string top;
@@ -210,49 +259,62 @@ void reduceOperators(stack<string>& operators, stack<int>& functionArgs, queue<s
 	}
 
 	output.push(top);
-	//std::cout << top << " ";
+	std::cout << top << " ";
 }
 
 //TODO:
-//1. cleaner code
-//2. own string
-//3. own stack
-//4. own queue
-//5. optimization
-void basicParsing(stack<string>& operators, queue <string>& output) {
+//1. own string
+//2. own stack
+//3. own queue
+//4. optimization
+
+/*
+* function that performs the Shunting yard algorithm. Basically it creates postfix notation of expression given in infix notation
+* @param operators - operators stack
+* @output - postfix notation expression
+*/
+void shuntingYard(stack<string>& operators, queue <string>& output) {
 
 	stack <int> functionArgs;
 	string currentToken;
 	while (true) {
 		cin >> currentToken;
+		// expression end
 		if (currentToken == ".") break;
 
 		if (isANumber(currentToken)) {
+			// for numbers just pushes the token to output
 			output.push(currentToken);
-			//std::cout << currentToken << " ";
+			std::cout << currentToken << " ";
 		}
 		else if (isAFuntion(currentToken)) {
+			// for functions just pushes the token to the operators stack
 			operators.push(currentToken);
 			functionArgs.push(1);
 		}
 		else if (currentToken == ",") {
+			// pops operators while the stack's top is not "(" and increases funtion's arguments count
 			while (operators.top() != "(") {
 				reduceOperators(operators, functionArgs, output);
 			}
 			functionArgs.top()++;
 		}
 		else if (currentToken == ")") {
+			// pops operators to output as long as the "(" is not at the stack's top
 			while (!operators.empty()) {
 				if (operators.top() == "(") break;
 				else {
 					reduceOperators(operators, functionArgs, output);
-
 				}
 			}
 			operators.pop();
 		}
 		else {
-			while (!operators.empty() && getPriority(operators.top()) >= getPriority(currentToken) && currentToken != "N" && operators.top() != "(") {
+			// rest of the operators
+			while (!operators.empty() &&
+				getPriority(operators.top()) >= getPriority(currentToken) &&
+				currentToken != "N" && /* checks for the operator being left-associative */
+				operators.top() != "(") {
 				reduceOperators(operators, functionArgs, output);
 
 			}
@@ -260,21 +322,22 @@ void basicParsing(stack<string>& operators, queue <string>& output) {
 		}
 
 	}
-
+	
+	// pops operators to output after the expression parsing
 	while (!operators.empty()) {
 		reduceOperators(operators, functionArgs, output);
 	}
+	std::cout << endl;
 }
 
+/*
+* funtion that handles every infix notation expression given by user
+*/
 void parseInfix() {
 	queue <string> output;
 	stack <string> operators;
-	string currentToken;
-	string top;
-	basicParsing(operators, output);
 
-
-	std::cout << endl;
+	shuntingYard(operators, output);
 	calculateRPN(output);
 }
 
